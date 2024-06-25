a87e527abc4661335b24f093100f99366c58dd06
晋阳 龙 <2736289464@qq.com>
2024-06-25 19:28:57 +0800

    项目骨架已搭建

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+ <div>
+ <div>Bot昵称：{{bot_name}}</div>
+ <div>Bot战力：{{bot_rating}}</div>
+ </div>
   <router-view/>
 </template>
 
+<script>
+import {ref} from 'vue';
+import $ from 'jquery';
+export default{
+  name: "app",
+  setup:()=>{
+    let bot_name=ref("");
+    let bot_rating=ref("");
+    $.ajax({
+      url:"http://localhost:9090/pk/getbotinfo/",
+      type: "get",
+      success: resp=>{
+        bot_name.value=resp.name;
+        bot_rating.value=resp.rating;
+      }
+    })
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
