1e789f04f5d7302349e4ce5f15e1bc5c55e02147
晋阳 龙 <2736289464@qq.com>
2024-07-25 10:31:41 +0800

    配置Mysql与注册登录模块（上)

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -1,10 +1,12 @@
 package com.kob.backend.controller.pk;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/pk/")
 public class IndexController {
-
+    @RequestMapping("index.html")
+    public String index(){return "0";}
 }
